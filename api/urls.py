239ec809dbf67fb7d72acf55621c08579e125bbc
farazkhanfk7 <farazkhan138@gmail.com>
2023-03-09 14:16:25 +0530

    slack

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('city', CityView.as_view()),
-    path('hook', SlackEventHookView.as_view()),
+    path('city/', CityView.as_view()),
+    path('hook/', SlackEventHookView.as_view()),
+    path('send/', SlackSendView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.conf import settings
 from .models import City
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.serializers import ModelSerializer
+import slack
+
+
+client = slack.WebClient(token=settings.BOT_USER_ACCESS_TOKEN)
 
 # Create your views here.
 class CityView(APIView):
@@ -18,6 +23,13 @@ class CityView(APIView):
         data = self.CitySerializer(cities, many=True).data
         return Response({'cities': data})
 
+
+class SlackSendView(APIView):
+    
+    def post(self, request, format=None):
+        data = request.data
+        client.chat_postMessage(channel='#test', text=data['text'])
+        return Response({'success': True})
 
 class SlackEventHookView(APIView):
     
